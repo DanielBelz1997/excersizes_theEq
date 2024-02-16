abb4dd42a272766d3ad291bb884a8cdadf63bacd
BelzDaniel1997 <BelzDaniel1997@gmail.com>
2024-02-16 14:39:30 +0200

    chpater 3 exersices

--- recursion.js
+++ recursion.js
@@ -13,3 +13,20 @@
 // a Boolean.
 // Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
 // way to fix this?
+
+function isEven(num) {
+  if (num < 0) {
+    console.log("not a valid num");
+    return false;
+  }
+  if (num === 0) {
+    console.log(true);
+  } else if (num === 1) {
+    console.log(false);
+  } else {
+    num = num - 2;
+    isEven(num);
+  }
+}
+
+isEven(6);
